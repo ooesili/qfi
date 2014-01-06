43488297c8d0f1be0d6763bfa1a7dfd13e216fdb
Wesley Merkel <ooesili@gmail.com>
2014-01-05 19:27:18 -0700

    Version bump: v0.2.0

--- lib/Qfi.pm
+++ lib/Qfi.pm
@@ -29,7 +29,7 @@ use File::Spec;
 BEGIN {
     require Exporter;
 
-    our $VERSION = "v0.1.0";
+    our $VERSION = "v0.2.0";
     our @ISA = qw(Exporter);
     #our @EXPORT = qw(add delete edit list move rename conf_dir);
     #our @EXPORT_OK = qw();
