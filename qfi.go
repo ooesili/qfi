d4e635f2347420a0da63128b742cfb2ccd249bf7
Wesley Merkel <ooesili@gmail.com>
2015-12-29 22:40:22 -0700

    Finish implementing main qfi command

--- qfi.go
+++ qfi.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
+	"path/filepath"
 
+	"github.com/ooesili/qfi/commands"
+	"github.com/ooesili/qfi/config"
+	"github.com/ooesili/qfi/detect"
 	"github.com/ooesili/qfi/dispatch"
+	"github.com/ooesili/qfi/edit"
+	"github.com/ooesili/qfi/summarize"
 )
 
 const usage = `Usage:
@@ -28,6 +35,11 @@ func main() {
 	if err := realMain(); err != nil {
 		fmt.Printf("qfi: %s\n", err)
 
+		// exit with status 2 if wrapper should chdir
+		if _, ok := err.(edit.WrapperShouldChdirError); ok {
+			os.Exit(2)
+		}
+
 		// print usage info is this is a UsageError
 		if _, ok := err.(dispatch.UsageError); ok {
 			fmt.Print(usage)
@@ -38,30 +50,45 @@ func main() {
 }
 
 func realMain() error {
+
+	// figure out config dir
+	var configDir string
+	if qfiHome := os.Getenv("QFI_CONFIGDIR"); qfiHome != "" {
+		configDir = qfiHome
+	} else if home := os.Getenv("HOME"); home != "" {
+		configDir = filepath.Join(home, ".config", "qfi")
+	} else {
+		return errors.New("neither $QFI_CONFIGDIR nor $HOME are set")
+	}
+
+	// figure out editor
+	var editorCmd string
+	if envEditor := os.Getenv("EDITOR"); envEditor != "" {
+		editorCmd = envEditor
+	} else {
+		editorCmd = "vi"
+	}
+
+	// initialize configuration
+	cfg, err := config.New(configDir)
+	if err != nil {
+		return err
+	}
+
+	// create dependencies
+	detector := detect.Detector{}
+	summarizer := summarize.Summarizer{detector, cfg}
+	editor := edit.Editor{editorCmd, detector, edit.RealExecutor{}, cfg}
+
 	// register commands
 	dispatcher := dispatch.New()
-	dispatcher.Register("-a", notImplemented)
-	dispatcher.Register("-m", notImplemented)
-	dispatcher.Register("-d", notImplemented)
-	dispatcher.Register("-r", notImplemented)
-	dispatcher.Register("-l", notImplemented)
-	dispatcher.Register("-s", notImplemented)
-	dispatcher.RegisterFallback(dispatch.CommandFunc(func(args []string) error {
-		// make sure there is exactly one argument
-		if len(args) < 1 {
-			return dispatch.UsageError("no targets given")
-		}
-		if len(args) > 1 {
-			return dispatch.UsageError("too many arguments")
-		}
-
-		return fmt.Errorf("target '%s' does not exist", args[0])
-	}))
+	dispatcher.Register("-a", &commands.Add{cfg})
+	dispatcher.Register("-m", &commands.Move{cfg})
+	dispatcher.Register("-d", &commands.Delete{cfg})
+	dispatcher.Register("-r", &commands.Rename{cfg})
+	dispatcher.Register("-l", &commands.List{cfg, os.Stdout})
+	dispatcher.Register("-s", &commands.Summary{summarizer, os.Stdout})
+	dispatcher.RegisterFallback(&commands.Edit{editor})
 
 	return dispatcher.Run(os.Args[1:])
 }
-
-var notImplemented = dispatch.CommandFunc(func(args []string) error {
-	fmt.Println("not implemented")
-	return nil
-})
