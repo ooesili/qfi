7a5786383ed58b76a01703f0d60512a733bbcd77
Wesley Merkel <ooesili@gmail.com>
2016-01-02 02:55:12 -0700

    Put New calls into a BeforeEach in config tests

--- config/config_test.go
+++ config/config_test.go
@@ -78,239 +78,203 @@ var _ = Describe("Config", func() {
 		})
 	})
 
-	Describe("Config.Resolve", func() {
-		Context("when given existing targets", func() {
-			It("can resolve their destinations", func() {
-				c, err := New(configDir)
-				Expect(err).ToNot(HaveOccurred())
+	Context("with a valid config struct", func() {
+		var c *Config
 
-				destination, err := c.Resolve("foobar")
-				Expect(err).ToNot(HaveOccurred())
-				Expect(destination).To(Equal("/foo/bar"))
-
-				destination, err = c.Resolve("bizbaz")
-				Expect(err).ToNot(HaveOccurred())
-				Expect(destination).To(Equal("/biz/baz"))
-
-				destination, err = c.Resolve("qux")
-				Expect(err).ToNot(HaveOccurred())
-				Expect(destination).To(Equal("/foo/bar/qux"))
-
-			})
-		})
-
-		Context("when given a nonexistent target", func() {
-			It("returns an error", func() {
-				c, err := New(configDir)
-				Expect(err).ToNot(HaveOccurred())
-
-				_, err = c.Resolve("badtarget")
-				Expect(err).To(MatchError("target 'badtarget' does not exist"))
-			})
-		})
-	})
-
-	Describe("Config.Add", func() {
-		It("can add a target when given an relative path", func() {
-			// add target
-			c, err := New(configDir)
-			Expect(err).NotTo(HaveOccurred())
-			err = c.Add("newtarget", "/dingus")
-			Expect(err).NotTo(HaveOccurred())
-
-			// read the target's symlink
-			destination, err := os.Readlink(filepath.Join(configDir, "newtarget"))
-			Expect(err).NotTo(HaveOccurred())
-
-			// make sure link points to the right place
-			Expect(destination).To(Equal("/dingus"))
-		})
-
-		It("can add a target when given an relative path", func() {
-			// add target
-			c, err := New(configDir)
-			Expect(err).NotTo(HaveOccurred())
-			err = c.Add("newtarget", "dingus")
-			Expect(err).NotTo(HaveOccurred())
-
-			// read the target's symlink
-			destination, err := os.Readlink(filepath.Join(configDir, "newtarget"))
-			Expect(err).NotTo(HaveOccurred())
-
-			// make sure link points to the right place
-			pwd, err := os.Getwd()
-			Expect(err).NotTo(HaveOccurred())
-			Expect(destination).To(Equal(filepath.Join(pwd, "dingus")))
-		})
-
-		Context("when config directory is read only", func() {
-			BeforeEach(func() {
-				err := os.Chmod(configDir, 0500)
-				Expect(err).NotTo(HaveOccurred())
-			})
-
-			AfterEach(func() {
-				err := os.Chmod(configDir, 0700)
-				Expect(err).NotTo(HaveOccurred())
-			})
-
-			It("returns an error", func() {
-				c, err := New(configDir)
-				Expect(err).NotTo(HaveOccurred())
-
-				err = c.Add("newtarget", "dingus")
-				Expect(err).To(HaveOccurred())
-				Expect(err.Error()).To(HavePrefix("cannot create symlink: %s: ",
-					filepath.Join(configDir, "newtarget")))
-			})
-		})
-	})
-
-	Describe("Config.List", func() {
-		It("lists all target names in alphabetical order", func() {
-			c, err := New(configDir)
+		BeforeEach(func() {
+			var err error
+			c, err = New(configDir)
 			Expect(err).ToNot(HaveOccurred())
-
-			Expect(c.List()).To(Equal([]string{
-				"bizbaz", "foobar", "qux",
-			}))
-		})
-	})
-
-	Describe("Config.Delete", func() {
-		Context("when given no targets", func() {
-			It("does nothing", func() {
-				c, err := New(configDir)
-				Expect(err).ToNot(HaveOccurred())
-
-				err = c.Delete()
-				Expect(err).ToNot(HaveOccurred())
-			})
 		})
 
-		Context("when given an existing target", func() {
-			It("removes that target", func() {
-				c, err := New(configDir)
-				Expect(err).ToNot(HaveOccurred())
-
-				err = c.Delete("foobar")
-				Expect(err).ToNot(HaveOccurred())
-
-				_, err = os.Readlink(filepath.Join(configDir, "foobar"))
-				Expect(err).To(HaveOccurred())
-			})
-		})
-
-		Context("when given multiple existing targets", func() {
-			It("removes all of the targets", func() {
-				c, err := New(configDir)
-				Expect(err).ToNot(HaveOccurred())
-
-				err = c.Delete("foobar", "bizbaz")
-				Expect(err).ToNot(HaveOccurred())
-
-				_, err = os.Readlink(filepath.Join(configDir, "foobar"))
-				Expect(err).To(HaveOccurred())
-
-				_, err = os.Readlink(filepath.Join(configDir, "bizbaz"))
-				Expect(err).To(HaveOccurred())
-			})
-		})
-
-		Context("when given a nonexistent target", func() {
-			It("returns an error and does not delete any given targets", func() {
-				c, err := New(configDir)
-				Expect(err).ToNot(HaveOccurred())
-
-				err = c.Delete("foobar", "badtarget")
-				Expect(err).To(MatchError("target 'badtarget' does not exist"))
-
-				_, err = os.Readlink(filepath.Join(configDir, "foobar"))
-				Expect(err).ToNot(HaveOccurred())
-			})
-		})
-	})
-
-	Describe("Config.Move", func() {
-		Context("when given an exsting target", func() {
-			Context("when given an absolute path", func() {
-				It("moves that target's destination", func() {
-					c, err := New(configDir)
+		Describe("Config.Resolve", func() {
+			Context("when given existing targets", func() {
+				It("can resolve their destinations", func() {
+					destination, err := c.Resolve("foobar")
 					Expect(err).ToNot(HaveOccurred())
+					Expect(destination).To(Equal("/foo/bar"))
 
-					err = c.Move("foobar", "/some/new/place")
+					destination, err = c.Resolve("bizbaz")
 					Expect(err).ToNot(HaveOccurred())
+					Expect(destination).To(Equal("/biz/baz"))
 
-					destination, err := os.Readlink(filepath.Join(configDir, "foobar"))
+					destination, err = c.Resolve("qux")
 					Expect(err).ToNot(HaveOccurred())
-					Expect(destination).To(Equal("/some/new/place"))
+					Expect(destination).To(Equal("/foo/bar/qux"))
+
 				})
 			})
 
-			Context("when given an absolute path", func() {
-				It("moves that target's destination", func() {
-					c, err := New(configDir)
-					Expect(err).ToNot(HaveOccurred())
-
-					err = c.Move("foobar", "relativetarget")
-					Expect(err).ToNot(HaveOccurred())
-
-					destination, err := os.Readlink(filepath.Join(configDir, "foobar"))
-					Expect(err).ToNot(HaveOccurred())
-
-					pwd, err := os.Getwd()
-					Expect(err).ToNot(HaveOccurred())
-					Expect(destination).To(Equal(filepath.Join(pwd, "relativetarget")))
-				})
-			})
-		})
-
-		Context("when given a nonexistent target", func() {
-			It("returns an error", func() {
-				c, err := New(configDir)
-				Expect(err).ToNot(HaveOccurred())
-
-				err = c.Move("badtarget", "/what/evs")
-				Expect(err).To(MatchError("target 'badtarget' does not exist"))
-			})
-		})
-	})
-
-	Describe("Config.Rename", func() {
-		Context("when given an existing target", func() {
-			It("renames the target", func() {
-				c, err := New(configDir)
-				Expect(err).ToNot(HaveOccurred())
-
-				err = c.Rename("foobar", "boofar")
-				Expect(err).ToNot(HaveOccurred())
-
-				_, err = os.Readlink(filepath.Join(configDir, "foobar"))
-				Expect(err).To(HaveOccurred())
-
-				destination, err := os.Readlink(filepath.Join(configDir, "boofar"))
-				Expect(err).ToNot(HaveOccurred())
-				Expect(destination).To(Equal("/foo/bar"))
-			})
-
-			Context("when the new name already exists", func() {
+			Context("when given a nonexistent target", func() {
 				It("returns an error", func() {
-					c, err := New(configDir)
-					Expect(err).ToNot(HaveOccurred())
-
-					err = c.Rename("foobar", "qux")
-					Expect(err).To(MatchError("target 'qux' exists"))
+					_, err := c.Resolve("badtarget")
+					Expect(err).To(MatchError("target 'badtarget' does not exist"))
 				})
 			})
 		})
 
-		Context("when given a nonexistent target", func() {
-			It("returns an error", func() {
-				c, err := New(configDir)
-				Expect(err).ToNot(HaveOccurred())
+		Describe("Config.Add", func() {
+			It("can add a target when given an relative path", func() {
+				// add target
+				err := c.Add("newtarget", "/dingus")
+				Expect(err).NotTo(HaveOccurred())
 
-				err = c.Rename("badtarget", "boofar")
-				Expect(err).To(MatchError("target 'badtarget' does not exist"))
+				// read the target's symlink
+				destination, err := os.Readlink(filepath.Join(configDir, "newtarget"))
+				Expect(err).NotTo(HaveOccurred())
+
+				// make sure link points to the right place
+				Expect(destination).To(Equal("/dingus"))
+			})
+
+			It("can add a target when given an relative path", func() {
+				// add target
+				err := c.Add("newtarget", "dingus")
+				Expect(err).NotTo(HaveOccurred())
+
+				// read the target's symlink
+				destination, err := os.Readlink(filepath.Join(configDir, "newtarget"))
+				Expect(err).NotTo(HaveOccurred())
+
+				// make sure link points to the right place
+				pwd, err := os.Getwd()
+				Expect(err).NotTo(HaveOccurred())
+				Expect(destination).To(Equal(filepath.Join(pwd, "dingus")))
+			})
+
+			Context("when config directory is read only", func() {
+				BeforeEach(func() {
+					err := os.Chmod(configDir, 0500)
+					Expect(err).NotTo(HaveOccurred())
+				})
+
+				AfterEach(func() {
+					err := os.Chmod(configDir, 0700)
+					Expect(err).NotTo(HaveOccurred())
+				})
+
+				It("returns an error", func() {
+					err := c.Add("newtarget", "dingus")
+					Expect(err).To(HaveOccurred())
+					Expect(err.Error()).To(HavePrefix("cannot create symlink: %s: ",
+						filepath.Join(configDir, "newtarget")))
+				})
+			})
+		})
+
+		Describe("Config.List", func() {
+			It("lists all target names in alphabetical order", func() {
+				Expect(c.List()).To(Equal([]string{
+					"bizbaz", "foobar", "qux",
+				}))
+			})
+		})
+
+		Describe("Config.Delete", func() {
+			Context("when given no targets", func() {
+				It("does nothing", func() {
+					err := c.Delete()
+					Expect(err).ToNot(HaveOccurred())
+				})
+			})
+
+			Context("when given an existing target", func() {
+				It("removes that target", func() {
+					err := c.Delete("foobar")
+					Expect(err).ToNot(HaveOccurred())
+
+					_, err = os.Readlink(filepath.Join(configDir, "foobar"))
+					Expect(err).To(HaveOccurred())
+				})
+			})
+
+			Context("when given multiple existing targets", func() {
+				It("removes all of the targets", func() {
+					err := c.Delete("foobar", "bizbaz")
+					Expect(err).ToNot(HaveOccurred())
+
+					_, err = os.Readlink(filepath.Join(configDir, "foobar"))
+					Expect(err).To(HaveOccurred())
+
+					_, err = os.Readlink(filepath.Join(configDir, "bizbaz"))
+					Expect(err).To(HaveOccurred())
+				})
+			})
+
+			Context("when given a nonexistent target", func() {
+				It("returns an error and does not delete any given targets", func() {
+					err := c.Delete("foobar", "badtarget")
+					Expect(err).To(MatchError("target 'badtarget' does not exist"))
+
+					_, err = os.Readlink(filepath.Join(configDir, "foobar"))
+					Expect(err).ToNot(HaveOccurred())
+				})
+			})
+		})
+
+		Describe("Config.Move", func() {
+			Context("when given an exsting target", func() {
+				Context("when given an absolute path", func() {
+					It("moves that target's destination", func() {
+						err := c.Move("foobar", "/some/new/place")
+						Expect(err).ToNot(HaveOccurred())
+
+						destination, err := os.Readlink(filepath.Join(configDir, "foobar"))
+						Expect(err).ToNot(HaveOccurred())
+						Expect(destination).To(Equal("/some/new/place"))
+					})
+				})
+
+				Context("when given an absolute path", func() {
+					It("moves that target's destination", func() {
+						err := c.Move("foobar", "relativetarget")
+						Expect(err).ToNot(HaveOccurred())
+
+						destination, err := os.Readlink(filepath.Join(configDir, "foobar"))
+						Expect(err).ToNot(HaveOccurred())
+
+						pwd, err := os.Getwd()
+						Expect(err).ToNot(HaveOccurred())
+						Expect(destination).To(Equal(filepath.Join(pwd, "relativetarget")))
+					})
+				})
+			})
+
+			Context("when given a nonexistent target", func() {
+				It("returns an error", func() {
+					err := c.Move("badtarget", "/what/evs")
+					Expect(err).To(MatchError("target 'badtarget' does not exist"))
+				})
+			})
+		})
+
+		Describe("Config.Rename", func() {
+			Context("when given an existing target", func() {
+				It("renames the target", func() {
+					err := c.Rename("foobar", "boofar")
+					Expect(err).ToNot(HaveOccurred())
+
+					_, err = os.Readlink(filepath.Join(configDir, "foobar"))
+					Expect(err).To(HaveOccurred())
+
+					destination, err := os.Readlink(filepath.Join(configDir, "boofar"))
+					Expect(err).ToNot(HaveOccurred())
+					Expect(destination).To(Equal("/foo/bar"))
+				})
+
+				Context("when the new name already exists", func() {
+					It("returns an error", func() {
+						err := c.Rename("foobar", "qux")
+						Expect(err).To(MatchError("target 'qux' exists"))
+					})
+				})
+			})
+
+			Context("when given a nonexistent target", func() {
+				It("returns an error", func() {
+					err := c.Rename("badtarget", "boofar")
+					Expect(err).To(MatchError("target 'badtarget' does not exist"))
+				})
 			})
 		})
 	})
